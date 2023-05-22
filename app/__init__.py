ea0cdfc2bc297216e76042431fd65a8375ba0633
Anna19531 <126623688+Anna19531@users.noreply.github.com>
2023-05-22 17:19:05 +1200

    Update __init__.py
    Renamed database to tasks.db

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///study.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 db = SQLAlchemy(app)
-
 
 from app import routes, models, forms
