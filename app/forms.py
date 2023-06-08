3f88d7ab80950d4148ec94c8d47f9d21e174e609
Anna19531 <126623688+Anna19531@users.noreply.github.com>
2023-06-09 10:07:52 +1200

    Added tasks table and form

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from app.models import Event
 from wtforms import IntegerField, TextAreaField, SelectField, validators, StringField, DateField
 from wtforms.validators import Optional, ValidationError, DataRequired
 
@@ -12,6 +13,20 @@ class EventForm(FlaskForm):
 def eventForm(request):
     form = EventForm(request.POST)
     form.colour.choices = [(1, "Red"), (2, "Blue"), (3, "Green"), (4, "Yellow"), (5, "Orange"), (6, "Purple"), (7, "Black")]
+
+class TaskForm(FlaskForm):
+    name = StringField("Task name", [validators.Length(min=2, max=144)])
+    description = StringField("Description", [validators.Length(min=2, max=500)])
+    date = DateField("Date")
+    type = SelectField("Type", [validators.Length(min=2, max=144)])
+    event = SelectField("Event", [validators.Length(min=2, max=144)])
+    #category = SelectField("Category", [validators.Length(min=2, max=144)])
+
+def taskForm(request):
+    form = TaskForm(request.POST)
+    form.type.choices = [(1, "School"), (2, "Personal")]
+    form.event.choices = [(event.id, event.name) for event in Event.query.all()]
+
 
 
 
--- app/models.py
+++ app/models.py
@@ -7,3 +7,12 @@ class Event(db.Model):
     description = db.Column(db.String(144), nullable=False)
     date = db.Column(db.Date, nullable=False)
     colour = db.Column(db.String(144), nullable=False)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(144), nullable=False)
+    description = db.Column(db.String(144), nullable=False)
+    date = db.Column(db.Date)
+    type = db.Column(db.String(144), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
